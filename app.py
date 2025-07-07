3f5759ab1e0ca3d3544182a87daa372948c6481b
Yashwanthgowdan <yashyashwanthgowdan@gmail.com>
2025-07-07 10:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from flask"
+    return "Hello from flask, parallel build"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
